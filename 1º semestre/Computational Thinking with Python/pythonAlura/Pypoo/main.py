4399eda15f2d63014c4531ef37493e063584a26d
devCaiodias <caiodiasmartins12@gmail.com>
2025-03-05 17:06:05 -0300

    Construtor e instanciando objetos

--- 1º semestre/Computational Thinking with Python/pythonAlura/Pypoo/main.py
+++ 1º semestre/Computational Thinking with Python/pythonAlura/Pypoo/main.py
@@ -1,14 +1,28 @@
+#  class em python
 class Retaurante:
-    nome = ''
-    categoria = ''
-    ativo = False
+    
+    restaurantes = []
 
-restaurante_praca = Retaurante()
-restaurante_praca.nome = 'Praça'
-restaurante_praca.categoria = 'Gourmet'
+    #  Todos atributos obrigatorio
+    def __init__(self, nome, categoria):
+        self.nome = nome
+        self.categoria = categoria
+        self.ativo = False
+        Retaurante.restaurantes.append(self)
 
-restaurante_pizza = Retaurante()
+    # Para transforma a resposta em texto
+    def __str__(self):
+        return f'{self.nome} e {self.categoria}'
+    
+    # Criando meu proprio metado
+    def listar_restaurante():
+        for restaurante in Retaurante.restaurantes:
+            print(f'{restaurante.nome} & {restaurante.categoria} & {restaurante.ativo}')
 
-restaurantes = [restaurante_praca, restaurante_pizza]
 
-print(vars(restaurante_praca))
+
+# Chamando a class com os atributos
+restaurante_praca = Retaurante('Praça', 'Gourmet')
+restaurante_pizza = Retaurante('Pizza', 'Italiana')
+
+Retaurante.listar_restaurante()
