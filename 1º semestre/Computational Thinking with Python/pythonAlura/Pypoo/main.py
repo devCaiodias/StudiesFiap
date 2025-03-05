5c0232fe2cbd73d0e85228011506caf14117cc2a
devCaiodias <caiodiasmartins12@gmail.com>
2025-03-05 18:11:14 -0300

    Property e métodos de classe

--- 1º semestre/Computational Thinking with Python/pythonAlura/Pypoo/main.py
+++ 1º semestre/Computational Thinking with Python/pythonAlura/Pypoo/main.py
@@ -5,24 +5,39 @@ class Retaurante:
 
     #  Todos atributos obrigatorio
     def __init__(self, nome, categoria):
-        self.nome = nome
-        self.categoria = categoria
-        self.ativo = False
+        self._nome = nome.title()
+        self._categoria = categoria.upper()
+        self._ativo = False
         Retaurante.restaurantes.append(self)
 
     # Para transforma a resposta em texto
     def __str__(self):
-        return f'{self.nome} e {self.categoria}'
+        return f'{self._nome} e {self._categoria}'
     
+    
+    @classmethod
     # Criando meu proprio metado
-    def listar_restaurante():
-        for restaurante in Retaurante.restaurantes:
-            print(f'{restaurante.nome} & {restaurante.categoria} & {restaurante.ativo}')
+    def listar_restaurante(cls):
+        print(f'{'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Status'}')
+        for restaurante in cls.restaurantes:
+            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante._ativo}')
+
+
+    @property
+    # metado get na teoria
+    def ativo(self):
+        return True if self._ativo else False
+    
+    # metado set na teoria
+    def alternar_estado(self):
+        self._ativo = not self._ativo
 
 
 
 # Chamando a class com os atributos
-restaurante_praca = Retaurante('Praça', 'Gourmet')
-restaurante_pizza = Retaurante('Pizza', 'Italiana')
+restaurante_praca = Retaurante('praça', 'Gourmet')
+# alterando o estado do ativo
+restaurante_praca.alternar_estado()
+restaurante_pizza = Retaurante('pizza', 'Italiana')
 
 Retaurante.listar_restaurante()
