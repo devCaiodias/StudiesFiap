5ad9c1cb3d3917aa40e5a9ed57b2ab7037228041
devCaiodias <caiodiasmartins12@gmail.com>
2025-03-05 15:23:31 -0300

    Utilizando métodos de strings

--- 1º semestre/Computational Thinking with Python/pythonAlura/strPython/main1.py
+++ 1º semestre/Computational Thinking with Python/pythonAlura/strPython/main1.py
@@ -1,11 +1,22 @@
 #  Url completa
-url = "bytebank.com/cambio?moedaOrigem=real"
-print(url)
+url = "bytebank.com/cambio?moedaDestino=dolar&moedaOrigem=real"
+# print(url)
 
 # Url Base
-url_base = url[0:19]
-print(url_base)
+indece_interrogacao = url.find('?')
+url_base = url[:indece_interrogacao]
+# print(url_base)
 
 # Paremetro da Url
-url_parametro = url[20:36]
+url_parametro = url[indece_interrogacao+1:]
 print(url_parametro)
+
+
+parametro_busca = 'moedaDestino'
+indice_parametro = url_parametro.find(parametro_busca)
+indice_valor = indice_parametro + len(parametro_busca) + 1
+valor = url_parametro[indice_valor:]
+
+print(valor)
+
+url.le
